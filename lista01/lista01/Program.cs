cb316a3e5b41bbb7efb22c2bdf13fa4891b48833
Vinicius-de-Morais <vmorais194@gmail.com>
2023-04-11 14:59:22 -0300

    fiz os exercicios que julguei mais dificeis

--- lista01/lista01/Program.cs
+++ lista01/lista01/Program.cs
@@ -10,16 +10,38 @@ public class teste
         Exercicio13();
     }
 
+    public static void exercicio11(){
+
+        Console.Write("Digite o número de eleitores: ");
+        int totalEleitores = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos brancos: ");
+        int votosBrancos = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos nulos: ");
+        int votosNulos = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos válidos: ");
+        int votosValidos = int.Parse(Console.ReadLine());
+
+        double percentualBrancos = (double)votosBrancos / totalEleitores * 100;
+        double percentualNulos = (double)votosNulos / totalEleitores * 100;
+        double percentualValidos = (double)votosValidos / totalEleitores * 100;
+
+        Console.WriteLine("Percentual de votos brancos: " + percentualBrancos.ToString("F2") + "%");
+        Console.WriteLine("Percentual de votos nulos: " + percentualNulos.ToString("F2") + "%");
+        Console.WriteLine("Percentual de votos válidos: " + percentualValidos.ToString("F2") + "%");
+    }
 
     public static void Exercicio12(){
 
         double marcacaoInicio, marcacaoFinal, combustivelGasto, valorRecebido, 
             totalKm, mediaConsumo, lucro;
 
-        Console.WriteLine("Digite a marcação do odômetro no início do dia (em Km): ");
+        Console.WriteLine("Digite a marcação no início do dia (em Km): ");
         marcacaoInicio = double.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite a marcação do odômetro no final do dia (em Km): ");
+        Console.WriteLine("Digite a marcação no final do dia (em Km): ");
         marcacaoFinal = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Digite a quantidade de combustível gasto (em litros): ");
@@ -37,7 +59,8 @@ public class teste
     }
 
     public static void Exercicio13()
-    {
+    {   
+        // Ler o salario minimo, o valor das bicicletas e a quantidade
         Console.WriteLine("Insira o salario do minimo");
         double salario = 2 * double.Parse(Console.ReadLine());
 
@@ -47,6 +70,7 @@ public class teste
         Console.WriteLine("Insira a quantidade de bicicletas");
         int bicicletasQuantity = int.Parse(Console.ReadLine());
 
+        // Fazer a l[ogica do calculo
         double aumento = (bicicletasValue / 0.15) * bicicletasQuantity;
 
         double trueSalary = salario + aumento;
